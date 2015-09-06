e58f4e058cf659e1525ba50e8db9c247f88a5539
Benjamin Lacroix <benlacroix83@gmail.com>
2015-09-06 14:28:34 +0100

    CollectionViewTest

---
+++ TestCollectionView/ButtonCell.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonCell.swift
+//  TestCollectionView
+//
+//  Created by Benobab on 06/09/15.
+//  Copyright © 2015 Benobab. All rights reserved.
+//
+
+import UIKit
+
+class ButtonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var button: UIButton!
+    
+    var test:Int!
+}
--- TestCollectionView/ViewController.swift
+++ TestCollectionView/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.backgroundColor = UIColor.whiteColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,3 +27,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UICollectionViewDataSource{
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell:ButtonCell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! ButtonCell
+        cell.backgroundColor = UIColor.whiteColor()
+        cell.button.titleLabel!.text = "button"
+        cell.button.titleLabel?.textColor = UIColor.whiteColor()
+        // Configure the cell
+        return cell
+    }
+}
+extension ViewController : UICollectionViewDelegate{
+ 
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+print("ok")
+    }
+}
