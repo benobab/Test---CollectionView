dfd7304413b8beaea7c4172ddef0e365d0c624e2
Benjamin Lacroix <benlacroix83@gmail.com>
2015-09-06 14:09:02 +0100

    Initial Commit

---
+++ TestCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCollectionView
+//
+//  Created by Benobab on 06/09/15.
+//  Copyright © 2015 Benobab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
